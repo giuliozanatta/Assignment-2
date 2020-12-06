eb9b5804564de34a0fd126dfa59e1379ee713b84
giuliozanatta <62381112+giuliozanatta@users.noreply.github.com>
2020-12-06 12:57:50 +0100

    Implementato 50% di sconto su gelato meno caro con test close #2

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -5,6 +5,7 @@ package it.unipd.tos.business;
 
 import java.util.List;
 import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.ItemType;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
 
@@ -12,15 +13,30 @@ public class TakeAwayBillImpl implements TakeAwayBill {
     
     public double getOrderPrice(List<MenuItem> itemsOrdered, User user) 
         throws RestaurantBillException {
-        
+        double numGelati = 0;
+        double min = 0;
         double tot = 0;
       
         for(int i=0; i<itemsOrdered.size(); i++) {
+            if(itemsOrdered.get(i).getItemType()==ItemType.Gelati) {
+                if(numGelati == 0) {
+                    min = itemsOrdered.get(i).getPrice();
+                }else {
+                    if(itemsOrdered.get(i).getPrice()<min) {
+                        min = itemsOrdered.get(i).getPrice();
+                    }
+                }
+                numGelati++;
+            }
                
                tot += itemsOrdered.get(i).getPrice();
         }     
+        
+        if(numGelati > 5) {
+            tot -= min/2;
+       }
            
-           return tot;
+        return tot;
     }
 }
      
